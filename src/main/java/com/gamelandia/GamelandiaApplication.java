276c7bc56269242fb4069684f231bcee2f318f86
Jonas <jonasdanova@gmail.com>
2023-10-05 16:51:11 -0300

    meu primeiro commit

---
+++ src/main/java/com/gamelandia/GamelandiaApplication.java
@@ -0,0 +1,13 @@
+package com.gamelandia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelandiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelandiaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gamelandia/model/Produtos.java
@@ -0,0 +1,107 @@
+package com.gamelandia.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+	 public class Produtos {
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@NotBlank(message = "O atributo nome é Obrigatório!")
+		@Size(min = 3, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres")
+		private String nome;
+		
+		@NotBlank(message = "O atributo descricao é Obrigatório!")
+		@Size(min = 10, max = 1000, message = "O atributo descricao deve conter no mínimo 10 e no máximo 1000 caracteres")
+		private String descricao;
+		
+		@NotNull(message = "O atributo quantidade é Obrigatório!")
+		@Positive
+		private Integer quantidade;
+		
+		@NotNull(message = "O atributo preço é Obrigatório!")
+		private BigDecimal preco;
+		
+		@NotBlank(message = "O atributo imagem é Obrigatório!")
+		private String imagem;
+
+	//Foreign Key
+		@ManyToOne
+		@JsonIgnoreProperties("Produtos")
+		private Categorias categorias;
+		
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getDescricao() {
+			return descricao;
+		}
+
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+
+		public Integer getQuantidade() {
+			return quantidade;
+		}
+
+		public void setQuantidade(Integer quantidade) {
+			this.quantidade = quantidade;
+		}
+
+		public BigDecimal getPreco() {
+			return preco;
+		}
+
+		public void setPreco(BigDecimal preco) {
+			this.preco = preco;
+		}
+
+		public String getImagem() {
+			return imagem;
+		}
+
+		public void setImagem(String imagem) {
+			this.imagem = imagem;
+		}
+
+		public Categorias getCategorias() {
+			return categorias;
+		}
+
+		public void setCategorias(Categorias categorias) {
+			this.categorias = categorias;
+		}
+		
+		
+		
+}
